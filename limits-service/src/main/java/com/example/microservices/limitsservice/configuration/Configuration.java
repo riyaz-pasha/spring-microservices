31d8ac60d7c94174ab7d4ed771408a437113a48f
riyaz <riyazpasha880@gmail.com>
2021-06-10 15:08:21 +0530

    Add. get limits from application configuration file

---
+++ limits-service/src/main/java/com/example/microservices/limitsservice/configuration/Configuration.java
@@ -0,0 +1,27 @@
+package com.example.microservices.limitsservice.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+    private double minimum;
+    private double maximum;
+
+    public double getMinimum() {
+        return minimum;
+    }
+
+    public void setMinimum(double minimum) {
+        this.minimum = minimum;
+    }
+
+    public double getMaximum() {
+        return maximum;
+    }
+
+    public void setMaximum(double maximum) {
+        this.maximum = maximum;
+    }
+}
--- limits-service/src/main/java/com/example/microservices/limitsservice/controller/LimitController.java
+++ limits-service/src/main/java/com/example/microservices/limitsservice/controller/LimitController.java
@@ -1,14 +1,19 @@
 package com.example.microservices.limitsservice.controller;
 
+import com.example.microservices.limitsservice.configuration.Configuration;
 import com.example.microservices.limitsservice.model.Limits;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitController {
 
+    @Autowired
+    private Configuration configuration;
+
     @GetMapping("/limits")
     public Limits retrieveLimits() {
-        return new Limits(1, 1000);
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
     }
 }
