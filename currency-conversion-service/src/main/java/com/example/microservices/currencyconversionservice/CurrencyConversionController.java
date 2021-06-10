fac36c10d541c03c505dffcec295a2f9bf3ab26b
riyaz <riyazpasha880@gmail.com>
2021-06-10 17:52:50 +0530

    created currency conversion rest controller

---
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,20 @@
+package com.example.microservices.currencyconversionservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
+    public CurrencyConversion createCurrencyConversion(
+            @PathVariable String from,
+            @PathVariable String to,
+            @PathVariable BigDecimal quantity
+    ) {
+        return new CurrencyConversion(1000L, from, to, quantity, BigDecimal.ONE, BigDecimal.ONE, "");
+    }
+}
